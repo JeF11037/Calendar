293a5387fb1f13aa9f4ed15e771f0c5cd4d4f02b
Лев Петряков <lev.petryakov@gmail.com>
2020-10-16 07:47:28 +0300

    Add png

--- Calendar/Calendar/months/March.xaml.cs
+++ Calendar/Calendar/months/March.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,25 @@ namespace Calendar
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class March : ContentPage
     {
+        Random rnd = new Random();
         public March()
         {
             InitializeComponent();
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            Image img = new Image();
+            img.Source = "spring.png";
+            img.Opacity = 1;
+            img.Scale = 3;
+            stack.Children.Add(img);
+            int tempX = Convert.ToInt32(DeviceDisplay.MainDisplayInfo.Width)/2;
+            await Task.WhenAll(
+                img.TranslateTo(0, -2000, 1),
+                img.TranslateTo(0, 500, 6000),
+                img.FadeTo(1, 4000)
+                );
         }
     }
 }
