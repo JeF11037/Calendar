746e5d56f9f49f506808a8804b19d11a00116ad7
Лев Петряков <lev.petryakov@gmail.com>
2020-10-21 01:12:27 +0300

    Add alert

--- Calendar/Calendar/MainPage.xaml.cs
+++ Calendar/Calendar/MainPage.xaml.cs
@@ -5,14 +5,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.WindowsSpecific;
 
 namespace Calendar
 {
-    public partial class MainPage : TabbedPage
+    public partial class MainPage : Xamarin.Forms.TabbedPage
     {
         public MainPage()
         {
             InitializeComponent();
+            AskAboutPage();
+        }
+
+        async void AskAboutPage()
+        {
+            string page = await DisplayActionSheet("What page do you want to see ?", "Cancel", null, "Spring", "Summer", "Autumn", "Winter");
+            switch (page)
+            {
+                default:
+                    break;
+                case "Spring":
+                    CurrentPage = Children[0];
+                    break;
+                case "Summer":
+                    CurrentPage = Children[1];
+                    break;
+                case "Autumn":
+                    CurrentPage = Children[2];
+                    break;
+                case "Winter":
+                    CurrentPage = Children[3];
+                    break;
+            }
         }
     }
 }
