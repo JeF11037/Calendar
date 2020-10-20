dc6bdf3a2e73ac85c8cc54cadd36d107fb057962
Лев Петряков <lev.petryakov@gmail.com>
2020-10-21 00:32:51 +0300

    Scale fix

--- Calendar/Calendar/months/April.xaml.cs
+++ Calendar/Calendar/months/April.xaml.cs
@@ -19,20 +19,53 @@ namespace Calendar
             InitializeComponent();
         }
 
-        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Appears();
+        }
+
+        private async void Appears()
+        {
+            mainImg.Opacity = 0;
+            await mainImg.FadeTo(1, 2000);
+        }
+
+        private async void ImageAnimation()
         {
             Image img = new Image();
             img.Source = images[rnd.Next(0, 3)];
-            img.Opacity = 1;
+            img.Opacity = 0;
             img.Scale = 3;
             stack.Children.Add(img);
             int tempX = Convert.ToInt32(DeviceDisplay.MainDisplayInfo.Width) / 2;
+            await img.TranslateTo(0, 0 - DeviceDisplay.MainDisplayInfo.Height / 2);
             await Task.WhenAll(
-                img.TranslateTo(0, -1000, 1),
-                img.TranslateTo(0, 500, 6000),
+                img.TranslateTo(0, 500, 6000, Easing.Linear),
                 img.FadeTo(1, 4000)
                 );
             stack.Children.Remove(img);
+        }
+
+        private async void CalendarAnimation(bool inverse)
+        {
+            if (inverse)
+            {
+                mainImg.Opacity = 1;
+                await mainImg.FadeTo(0, 2000);
+            }
+            else
+            {
+                mainImg.Opacity = 0;
+                await mainImg.FadeTo(1, 2000);
+            }
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            CalendarAnimation(true);
+            ImageAnimation();
+            CalendarAnimation(false);
         }
     }
 }
